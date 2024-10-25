00b2459d70e31ce372031af011458eae2f9977bf
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-10-25 11:00:49 +0530

    added data to db

--- routes/categories.js
+++ routes/categories.js
@@ -3,16 +3,43 @@ const router = express.Router();
 
 const Category = require("../models/category.models");
 
+//  get endpoints
 router.get('/', async(req,res)=>{
     try{
         const categories = await Category.find();
         if(categories.length > 0){
             res.status(200).json({data: {categories}});
         }else{
-            res.status(404).json({message:'categories list is empty'});
+            res.status(204);
         }
     }catch(error){
         res.status(500).json({message:error.message})
+    }
+})
+
+router.get('/:categoryId', async(req,res)=>{
+    try{
+        const id = req.params.categoryId;
+        const category = await Category.findById(id);
+        if(category){
+            res.status(200).json(category);
+        }else{
+            res.status(404).json({message:`Category not found.`})
+        }
+    }catch(error){  
+        res.status(500).json({error:error.message});
+    }
+})
+
+
+// post endpoints
+router.post('/', async(req,res)=>{
+    try{
+        const category = new Category(req.body);
+        await category.save();
+        res.status(201).json({category:category})
+    }catch(error){
+        res.status(500).json({error:error.message});
     }
 })
 
--- routes/products.js
+++ routes/products.js
@@ -3,14 +3,42 @@ const router = express.Router();
 
 const Product = require("../models/product.models");
 
+//  get endpoints
+
 router.get('/', async(req,res)=>{
     try{
         const products = await Product.find();
         if(products.length > 0){
             res.status(200).json({data:{products}})
         }else{
-            res.status(404).json({message:'Products list is empty'})
+            res.status(204);
         }
+    }catch(error){
+        res.status(500).json({error:error.message});
+    }
+})
+
+router.get('/:productId', async(req,res)=>{
+    try{
+        const id = req.params.productId;
+        const product = await Product.findById(id);
+        if(product){
+            res.status(200).json(product);
+        }else{
+            res.status(404).json({message:`Product not found.`})
+        }
+    }catch(error){  
+        res.status(500).json({error:error.message});
+    }
+})
+
+// post endpoints
+
+router.post('/', async(req,res)=>{
+    try{
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).json({product:product})
     }catch(error){
         res.status(500).json({error:error.message});
     }
